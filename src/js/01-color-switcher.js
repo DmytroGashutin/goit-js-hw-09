a6a1d297f137a0f8c83d761625fb49d2be74f9fc
Dmytro Gashutin <dm.gashutin@gmail.com>
2022-04-04 23:36:24 +0300

    tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let timeIntervalId = null;
+const isDisabled  = true;
+
+refs.stopBtn.disabled = isDisabled;
+
+function changeBgc(){
+    document.body.style.backgroundColor = getRandomHexColor();
+};
+
+
+refs.startBtn.addEventListener('click', () => {
+
+    timeIntervalId = setInterval(changeBgc, 1000);
+    refs.startBtn.disabled = isDisabled;
+    refs.stopBtn.disabled = !isDisabled;
+   
+});
+
+refs.stopBtn.addEventListener('click', () => {
+
+   
+    clearInterval(timeIntervalId);
+     refs.startBtn.disabled = !isDisabled;
+    refs.stopBtn.disabled = isDisabled;
+});
